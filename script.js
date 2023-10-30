25f12e9e63e51edf0b99ef2e6fbe84444dd3cd02
Deligent2 <148380705+Deligent2@users.noreply.github.com>
2023-10-31 00:22:17 +0300

    finished

---
+++ script.js
@@ -0,0 +1,99 @@
+// function myFunction() {
+//     let a = 'User'
+
+//     if (a.toLowerCase() === 'user') {
+//         return 'user admin welcome'
+//     }
+//     else {
+//         return 'not correct user'
+//     }
+// }
+
+// console.log(myFunction())
+
+// function myFunction(day) {
+
+//     if (day >= 1 && day <= 28) {
+//         return `to day is ${day} of february`
+//     } else {
+//         return `on february only 28 days, your day is ${day} of another month`
+//     }
+// }
+
+// console.log(myFunction(29))
+
+// function myFunction(num) {
+//     if (num >= 50 && num <= 50) {
+//         return `${num} equals of 50`
+//     } else {
+//         return `${num} is not correct`
+//     }
+// }
+
+// console.log(myFunction(60))
+
+// function myFunction(user, age) {
+
+//     if (age >= 18 && age <= 50 ){
+//         return `${user} ${age} old, welcome`
+//     } else {
+//         return `${user} ${age} old, rejected`
+//     }
+// }
+
+// console.log(myFunction("kuba", 17))
+
+// function myFunction(user) { 
+//     if (user.length <= 3) { 
+//         return `${user} name is too short`; 
+//     } else { 
+//         return `${user} name is accepted`; 
+//     } 
+// } 
+
+// console.log(myFunction(''));
+
+// function myFunction(user) {
+//     if (user.length >= 5) {
+//         return `${user} name is too long`;
+//     } else {
+//         return `${user} name is accepted`;
+//     }
+// }
+
+// console.log(myFunction(''));
+
+// function myFunction(day) {
+
+//     if (day >= 1 && day <= 7) {
+//         return `today is ${day} weekend`
+//     } else {
+//         return 'max days of weeks 7'
+//     }
+// }
+// console.log(1)
+
+// function getDayOfWeek(today) {
+//     let dayOfWeek;
+
+//     if (today === 7 || today === 6) {
+//         dayOfWeek = "weekend";
+//     } else if (today === 1) {
+//         dayOfWeek = "Monday";
+//     } else if (today === 2) {
+//         dayOfWeek = "Tuesday";
+//     } else if (today === 3) {
+//         dayOfWeek = "Wednesday";
+//     } else if (today === 4) {
+//         dayOfWeek = "Thursday";
+//     } else if (today === 5) {
+//         dayOfWeek = "Friday";
+//     } else {
+//         return 'max days of weeks 7';
+//     }
+
+//     return 'Today is ' + dayOfWeek;
+// }
+
+// console.log(getDayOfWeek(7));
+
